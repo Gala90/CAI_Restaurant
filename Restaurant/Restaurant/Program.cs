b54630fcf2fe42160373c6f5c094cdc6b3b05b94
Gala <galamagdalena@hotmail.com>
2020-03-27 21:01:19 -0300

    Atributos

---
+++ Restaurant/Restaurant/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+
+    public class Restaurant : Attribute
+    {
+        public String nombre;
+        public String direccion;
+        public String telefono;
+        public int cantMesas;
+        public string[] mesas;
+        public string[] menu;
+
+        public Restaurant (string nombre, string telefono, string direccion, int cantMesas)
+        {
+            this.nombre = nombre;
+            this.direccion =  direccion;
+            this.telefono = telefono;
+            this.cantMesas = cantMesas;
+
+
+        }
+
+
+
+    }
+  
+}
